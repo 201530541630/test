cf9b8246ee60d0ff13ed1de068b6803865e792d3
YUE FANG <fy0715eien@gmail.com>
2018-03-05 13:02:15 +0800

    为MainActivity加注释

--- app/src/main/java/com/example/administrator/lab2_2/MainActivity.java
+++ app/src/main/java/com/example/administrator/lab2_2/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.administrator.lab2_2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+/*Edited by fy*/
 public class MainActivity extends AppCompatActivity {
 
     @Override
